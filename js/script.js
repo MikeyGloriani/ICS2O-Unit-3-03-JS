14c3735764b45eee2ccc69031b82a5cb0c56c132
MikeyGloriani <MikeyGloriani@users.noreply.github.com>
2022-11-02 18:11:05 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -16,12 +16,12 @@ if (navigator.serviceWorker) {
 /**
  * This function calculates the volume of a sphere
  */
- function calculate() {
+function calculate() {
   // input
   const radius = parseFloat(document.getElementById("radius").value)
 
   // process
-  const Volume = (4/3)*(Math.PI) * (radius**3)
+  const Volume = (4 / 3) * Math.PI * radius ** 3
 
   // output
   document.getElementById("volume").innerHTML = "Volume is: " + Volume + " cm²"
